a15d24a5613fc8b4756888d4016aba130e1f4964
Yasir Faizan <yasirfaizan.web@gmail.com>
2025-03-16 13:09:01 +0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+let Distance = Number(prompt("Please Enter Distance in KM"))
+if(Distance<=5 && Distance >=0.1){
+    console.log(alert("Amount is 15"))
+}
+else if(Distance<=0){
+    console.log(alert("Distance Never Be Zero Or Less Than Zero"))
+}
+else if(Distance>=5.1 && Distance <=10){
+    console.log(alert("Amount is 20"))
+}
+else if(Distance>=10.1 && Distance <=15){
+    console.log(alert("Amount is 25"))
+}
+else if(Distance>=15.1 && Distance <=20){
+    console.log(alert("Amount is 30"))
+}
+else if(Distance>=20.1 && Distance <=25){
+    console.log(alert("Amount is 35"))
+}
+else if(Distance>=25.1 && Distance <=30){
+    console.log(alert("Amount is 40"))
+}
+else if(Distance>=30.1 && Distance <=35){
+    console.log(alert("Amount is 50"))
+}
+else if(Distance>=40.1 && Distance <=45){
+    console.log(alert("Amount is 60"))
+}
+else(
+    console.log(alert("BRT Line IS Not Long Enough"))
+)
